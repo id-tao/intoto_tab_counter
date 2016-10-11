c7b05a5d85a5b536839ed0170f9a120e0617fda0
tao-io1 <tao@io1.co>
2016-10-11 09:23:56 +0300

    #78 - intoto_tab_counter module to add counter into the tab title.

---
+++ intoto_tab_counter.api.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Alter the tab counter before it gets rendered.
+ *
+ * @param &$tab_link
+ *   A tab link options, either from code.
+ */
+function hook_tab_count_items($tab_link) {
+}
+
